62682d7ba9ef91cb717e6b7b2e1b1ae6bc04bb39
videogamehover <72026191+VideoGameHover@users.noreply.github.com>
2021-03-16 19:49:11 -0500

    sub to vgh

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+//this is the code for when the bot is ready
+//ok fine ill shut up
+
 module.exports = (client) => {
   console.log(`${client.config.botName} is online.`);
 	client.user.setActivity(client.config.activityStatus, { type: client.config.activityType })
