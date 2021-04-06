b44d5ab3da69609351ded6f4dc3d3d957e03820b
TheOriginalCoder620 <72026191+vghcodesjs@users.noreply.github.com>
2021-04-06 10:18:00 -0500

    no more indain

--- commands/about.js
+++ commands/about.js
@@ -1,11 +1,10 @@
 const Discord = require('discord.js');
 const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#FFA500')
-	.setTitle('About the Indian Bot')
+	.setTitle('Put title')
 	.setAuthor('by videogamehover ', 'https://lh3.googleusercontent.com/a-/AOh14Ghn-t2X4i6h6q4EDSNRq52CwMfOhmv-VaQ_Ou3-eg=s600-k-no-rp-mo', 'https://www.youtube.com/channel/UC6bYYwrw9kukEMYzgkrdZmw')
-	.setDescription('Sup Bots. I\'m Josh, and I created this bot to humorize people in all ways. ')
-	.setThumbnail('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiMi0n5wkkrEJPQMGcGhyoabRJSRS3KTveVA&usqp=CAU')
-	.setFooter('Coded using javascript');
+	.setDescription('Set a description')
+        .setFooter('Put a footer');
 
 module.exports.run = async (bot, message, args) => {
     message.reply(exampleEmbed);
