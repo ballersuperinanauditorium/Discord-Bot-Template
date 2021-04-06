e2f93b6538c50eff40a5625a65cd733cbcb00ad3
TheOriginalCoder620 <72026191+vghcodesjs@users.noreply.github.com>
2021-04-06 10:18:30 -0500

    insert

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#FFA500')
 	.setTitle('Hello, please help me I am under the water!')
 	.setAuthor('Help Command')
-	.setDescription('Here are the list of commands: \n``i-help : brings up this panel``\n ``i-about : more about the bot`` \n ``i-meme : brings up an indian meme`` \n ``i-rmeme : brings up a meme from reddit``')
+	.setDescription('Here are the list of commands: INSERT YOUR COMMANDS')
 	.setFooter('bruh u need help?');
 
     module.exports.run = async (bot, message, args) => {
